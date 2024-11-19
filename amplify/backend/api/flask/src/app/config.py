b52e5de0f31d1723513914321827073bba9fba30
Bryn Deering <bdeering@uoguelph.ca>
2024-11-19 17:34:05 -0500

    First working amplify deployment
    - initialized amplify (gen 1) project with custom docker backend
    - modified docker compose file to work for ECS deployment
      - explicitly specified app environment variables in docker-compose
      - now using compose secrets stored in ../secrets/.secret-* files to utilize AWS secret manager
      - db service uses ecr version of postgres image
    - config.py can now pull variables from secret files

--- amplify/backend/api/flask/src/app/config.py
+++ amplify/backend/api/flask/src/app/config.py
@@ -1,14 +1,19 @@
 from os import environ
 from dotenv import load_dotenv
 
+
+def get_secret(name):
+    return environ.get(name) or open(f'/run/secrets/{name}').read().rstrip('\n')
+
+
 load_dotenv()
 
 POSTGRES_DB = environ.get('POSTGRES_DB')
 POSTGRES_USER = environ.get('POSTGRES_USER')
-POSTGRES_PASSWORD = environ.get('POSTGRES_PASSWORD')
+POSTGRES_PASSWORD = get_secret('POSTGRES_PASSWORD')
 POSTGRES_HOST = environ.get('POSTGRES_HOST')
 POSTGRES_PORT = environ.get('POSTGRES_PORT')
 
-OPENAI_API_KEY = environ.get('OPENAI_API_KEY')
+OPENAI_API_KEY = get_secret('OPENAI_API_KEY')
 ORGANIZATION_ID = environ.get('ORGANIZATION_ID')
 PROJECT_ID = environ.get('PROJECT_ID')
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,12 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "flask": {
+      "ApiName": "string",
+      "ClusterName": "string",
+      "ContainerNames": "string",
+      "PipelineName": "string",
+      "RootUrl": "string",
+      "ServiceName": "string"
+    }
+  }
+}
